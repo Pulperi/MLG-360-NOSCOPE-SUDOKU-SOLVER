7fcf122acf823040e74f8dfa629521ef8ae133fb
Pulperi <pulperi@gmail.com>
2015-01-23 09:24:26 +0200

    fileio, ui

---
+++ SudokuWorldSaga/src/main/java/sudokuproject/sudokuworldsaga/fileio/FileManager.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudokuproject.sudokuworldsaga.fileio;
+
+/**
+ *
+ * @author Henri
+ */
+public class FileManager {
+    
+}
---
+++ SudokuWorldSaga/src/main/java/sudokuproject/sudokuworldsaga/ui/UIManager.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudokuproject.sudokuworldsaga.ui;
+
+/**
+ *
+ * @author Henri
+ */
+public class UIManager {
+    
+}
